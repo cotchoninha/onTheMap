c39cdd5ff07451dad80265ee8b0e92a0369bb028
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-06-04 22:58:36 +0100

    create the Login UI, the LoginViewController, added code for the login button and sign in button

---
+++ OnTheMap/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  OnTheMap
+//
+//  Created by Marcela Ceneviva Auslenter on 04/06/2018.
+//  Copyright © 2018 Marcela Ceneviva Auslenter. All rights reserved.
+//
+
+import Foundation
+
+// MARK: Constants
+struct Constants {
+    
+    // MARK: API Key
+    //You will be required to specify the Parse Application ID and REST API Key for all requests to the Parse API. 
+    static let ApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+    static let ParseApplicationID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+    //session
+    
+}
---
+++ OnTheMap/LoginVC.swift
@@ -0,0 +1,103 @@
+//
+//  LoginVC.swift
+//  OnTheMap
+//
+//  Created by Marcela Ceneviva Auslenter on 04/06/2018.
+//  Copyright © 2018 Marcela Ceneviva Auslenter. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class LoginVC: UIViewController{
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        
+        usernameTextField.text = "cotchoninha@yahoo.com.br"
+        passwordTextField.text = "lulu1234"
+        
+        var request = URLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+        
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"udacity\": {\"username\": \"\(usernameTextField.text!)\", \"password\": \"\(passwordTextField.text!)\"}}".data(using: .utf8)
+       
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            
+            func sendError(_ error: String) {
+                print(error)
+            }
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                sendError("There was an error with your request: \(error!)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
+                sendError("Your request returned a status code other than 2xx!")
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                sendError("No data was returned by the request!")
+                return
+            }
+            
+            let range = Range(5..<data.count)
+            let newData = data.subdata(in: range) /* subset response data! */
+            print("AQUI COMECA A IMPRIMIR O DATA!!!!")
+            print(String(data: newData, encoding: .utf8)!)
+
+            /* 5. Parse the data */
+            let parsedResult: [String:AnyObject]!
+            do{
+                parsedResult = try JSONSerialization.jsonObject(with: newData, options: .allowFragments) as? [String:AnyObject]
+            } catch{
+                sendError("Could not parse the data as JSON: '\(newData)'")
+                return
+            }
+            
+            guard let dictionarySession = parsedResult["session"] else{
+                sendError("Cannot find keys 'session' in \(parsedResult)")
+                return
+            }
+            if let sessionID = dictionarySession["id"] as? String{
+                print("sessionID: \(sessionID)")
+                DispatchQueue.main.async {
+                 (UIApplication.shared.delegate as! AppDelegate).sessionID = sessionID
+                }
+            }else{
+                print("could not find a SessionID.")
+            }
+        }
+        
+        task.resume()
+        
+    }
+    
+    @IBAction func SignInButton(_ sender: Any) {
+        
+        let udacityWebSite = URL(string: "https://www.udacity.com/account/auth#!/signup")!
+        UIApplication.shared.open(udacityWebSite, options: [:], completionHandler: nil)
+        
+    }
+    
+    @IBAction func facebookSignin(_ sender: Any) {
+    }
+    
+    
+}
+
+
