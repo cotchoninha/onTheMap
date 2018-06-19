959ec2b93f98857377f7a932034a5099108b49c2
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-06-19 11:48:20 +0100

    updated MAP VC with ParseAPIgetfunction

--- controler/MapVC.swift
+++ controler/MapVC.swift
@@ -12,11 +12,24 @@ import CoreLocation
 
 class MapVC: UIViewController, MKMapViewDelegate{
     
+    //MARK: Properties
     @IBOutlet weak var mapView: MKMapView!
+    var parseAPIClient = ParseAPIClient()
+    var allStudents = [Student]()
+    var annotations = [MKPointAnnotation]()
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        getStudendLocation()
+        parseAPIClient.getStudendLocation {(success, studentsArray, error) in
+            if success{
+                performUIUpdatesOnMain {
+                    self.allStudents = studentsArray!
+                    self.addStudentLocationToMap()
+                }
+            }else{
+                //tratar o erro
+            }
+        }
     }
     
     override func viewDidLoad() {
@@ -25,74 +38,11 @@ class MapVC: UIViewController, MKMapViewDelegate{
         mapView.delegate = self
     }
     
-    var allStudents = [Student]()
-    var annotations = [MKPointAnnotation]()
-    
-    func checkForErrors(data: Data?, response: URLResponse?, error: Error?){
-        func sendError(_ error: String) {
-            print(error)
-        }
-        /* GUARD: Was there an error? */
-        guard (error == nil) else {
-            sendError("There was an error with your request: \(error!)")
-            return
-        }
-        
-        /* GUARD: Did we get a successful 2XX response? */
-        guard let statusCode = (response as? HTTPURLResponse)?.statusCode, statusCode >= 200 && statusCode <= 299 else {
-            sendError("Your request returned a status code other than 2xx!")
-            return
-        }
-        
-        /* GUARD: Was there any data returned? */
-        guard let data = data else {
-            sendError("No data was returned by the request!")
-            return
-        }
-    }
-    
-    func parseDataWithCodable(data: Data) -> [Student]? {
-        
-        let jsonDecoder = JSONDecoder()
-        do{
-            let studentsResult = try jsonDecoder.decode(StudentResult.self, from: data)
-            return studentsResult.results
-        } catch{
-            print("Could not parse the data as JSON: '\(data)'")
-            return nil
-        }
-    }
-    
-    func getStudendLocation(){
-        
-        var request = URLRequest(url: URL(string: "https://parse.udacity.com/parse/classes/StudentLocation?limit=100&order=-updatedAt")!)
-        request.addValue("QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr", forHTTPHeaderField: "X-Parse-Application-Id")
-        request.addValue("QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY", forHTTPHeaderField: "X-Parse-REST-API-Key")
-        let session = URLSession.shared
-        let task = session.dataTask(with: request) { data, response, error in
-            
-            self.checkForErrors(data: data, response: response, error: error)
-            
-            if let studentsList = self.parseDataWithCodable(data: data!){
-                self.allStudents = studentsList
-            }
-            performUIUpdatesOnMain {
-                self.addStudentLocationToMap()
-            }
-        }
-        task.resume()
-    }
-    
     func addStudentLocationToMap(){
         
-        
         for student in allStudents {
-//            guard student.longitude != nil else{
-//                return
-//            }
-            // se ltitude e long estiverem vazios nao adiciona
             if student.latitude == nil && student.longitude == nil{
-                //nao adiciona
+                //TODO: nao adiciona
             } else{
                 let latitude = CLLocationDegrees(student.latitude!)
                 let longitude = CLLocationDegrees(student.longitude!)
@@ -106,9 +56,7 @@ class MapVC: UIViewController, MKMapViewDelegate{
                 annotation.subtitle = student.mediaURL ?? ""
                 
                 // Finally we place the annotation in an array of annotations.
-                
                 annotations.append(annotation)
-                
             }
         }
         self.mapView.addAnnotations(annotations)
@@ -140,7 +88,4 @@ class MapVC: UIViewController, MKMapViewDelegate{
             }
         }
     }
-    
-    
-    
 }
