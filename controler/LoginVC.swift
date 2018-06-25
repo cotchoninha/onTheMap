3e63af9df5d8dd482bc20bcdbeed20bbcedf420c
Marcela Ceneviva Auslenter <cotchoninha@yahoo.com.br>
2018-06-25 14:05:36 +0100

    inserted activity indicator in LoginVC

--- controler/LoginVC.swift
+++ controler/LoginVC.swift
@@ -14,10 +14,13 @@ import FacebookCore
 
 class LoginVC: UIViewController, LoginButtonDelegate{
     
+    
     //MARK: Properties
     let udacityClient = UdacityAPIClient()
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    @IBOutlet weak var loginButtonOutlet: UIButton!
     
     //TODO:
     func loginButtonDidLogOut(_ loginButton: LoginButton) {
@@ -26,6 +29,9 @@ class LoginVC: UIViewController, LoginButtonDelegate{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        activityIndicator.isHidden = true
+        usernameTextField.text = "cotchoninha@yahoo.com.br"
+        passwordTextField.text = "lulu1234"
         let loginButton = LoginButton(readPermissions: [ .publicProfile ])
         loginButton.delegate = self
         view.addSubview(loginButton)
@@ -85,20 +91,29 @@ class LoginVC: UIViewController, LoginButtonDelegate{
     
     //MARK: LOGIN WITH UDACITY USER
     @IBAction func loginButton(_ sender: Any) {
+        //start activity indicator
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        loginButtonOutlet.isEnabled = false
         
         //check if username and password are not empty
         if  usernameTextField.text == "" || usernameTextField.text == nil{
             UserAlertManager.showAlert(title: "Invalid username", message: "Please, type a valid username.", buttonMessage: "Try again.", viewController: self)
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
             return
         }
         if passwordTextField.text == "" || passwordTextField.text == nil{
             UserAlertManager.showAlert(title: "Invalid password", message: "Please, type a valid password.", buttonMessage: "Try again.", viewController: self)
+            activityIndicator.stopAnimating()
+            activityIndicator.isHidden = true
             return
         }
         
         //call a function that will make the login request passing a completion handler for the response
         udacityClient.userLoginRequest(username: usernameTextField.text!, password: passwordTextField.text!) { (success, sessionID, keyAccount, error) in
             performUIUpdatesOnMain {
+                self.activityIndicator.stopAnimating()
                 if success{
                     //caso login tenha dado certo
                     print("MARCELA - sessionID: \(sessionID) and accountKey: \(keyAccount)")
@@ -117,6 +132,8 @@ class LoginVC: UIViewController, LoginButtonDelegate{
                         print("an error has occured")
                         UserAlertManager.showAlert(title: "Login failed.", message: "We couldn't access your account. Try again.", buttonMessage: "Try again.", viewController: self)
                     }
+                    self.activityIndicator.stopAnimating()
+                    self.activityIndicator.isHidden = true
                 }
             }
         }
@@ -130,7 +147,4 @@ class LoginVC: UIViewController, LoginButtonDelegate{
     }
     
 }
-
-
-
 
